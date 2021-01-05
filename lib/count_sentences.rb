77dc82a0da2250c2b4648d0c017682c2f7a71fc2
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-05 19:42:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,15 +19,6 @@ class String
     string_array = self.split(/\.|!|\?/)
     updated_array = string_array.reject { |c| c.empty?}
     puts updated_array 
-    #complex_String = /$|!/
-  
-    
-    # puts complex_String 
-    # if complex_String.include? ("") || complex_String.end_with?("  ")
     return updated_array.length
 end
 end
-
-
-#\z - Matches end of string 
-#$ end of sentence 
